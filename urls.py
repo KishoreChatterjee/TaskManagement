aba8ad44ed6297053ea98eaabcdd65d0485d9173
KishoreChatterjee <119159094+KishoreChatterjee@users.noreply.github.com>
2024-01-12 16:15:17 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -4,8 +4,15 @@ from .views import *
 urlpatterns = [
     path('register/', register),
     path('login/', login),
-    path("createBoard/",createBoard),
-    path('createCard/',createCard),
-    path('createList/',createList)
-
+    path("createBoard/", createBoard),
+    path('createCard/', createCard),
+    path('createListTitle/', createListTitle),
+    path('tasks/', createTask),
+    path('addPersonToOranistation/',addPersonToOranistation),
+    path('addPersonToBoard/',addPersonToBoard),
+    path('addUserToCard/',addUserToCard),
+    path('removeUserFromOrganisation/',removeUserFromOrganisation),
+    path('updateBoardName/',updateBoardName),
+    path('removeBoard/',removeBoard),
+    path('makeCardInactive/',makeCardInactive),
 ]
